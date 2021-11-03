7fe02a57ede1b023b259d945783bf54893da45db
Huilén Abed <huilen.abed@gmail.com>
2021-11-02 22:29:46 -0300

    add missing dependencies

--- src/main.py
+++ src/main.py
@@ -63,5 +63,5 @@ async def find_node(params: FindNodeRequestModel):
     return node
 
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+if __name__ == '__main__':
+    uvicorn.run(app, host='0.0.0.0', port=8000)
